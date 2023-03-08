4d3a6d1866e383a2709cdd8cff25e04cfef56145
NightAdrian <117701587+NightAdrian@users.noreply.github.com>
2023-03-08 15:05:32 -0800

    Add files via upload

---
+++ InitialsPractice.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+	// I like pokeon :D 
+	cout << "   A\n";
+	cout << "  A  A\n";
+	cout << " A     A\n";
+	cout << "A A A A A\n";
+	cout << "A       A\n";
+	cout << "A       A\n";
+	cout << "A       A \n";
+
+	cout << " R R R R \n";
+	cout << " R       R\n";
+	cout << " R       R\n";
+	cout << " R R R R \n";
+	cout << " R       R\n";
+	cout << " R        R\n";
+	cout << " R         R\n";
+
+	system("Pause");
+	return 0;
+}
