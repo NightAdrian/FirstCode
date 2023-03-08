86ac5c1dfdbb3182f18609d41f1a7d6e48ec4ed1
NightAdrian <117701587+NightAdrian@users.noreply.github.com>
2023-03-08 14:43:04 -0800

    Add files via upload

---
+++ SimpleInputNumber.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int  main()
+{
+
+	 int x;
+	cout << " Hello world! ";
+
+	cout << " If you could be so kind and enter a number : ";
+	cin >> x;
+	cout << "Your Number is  = " << x 
+		<< endl 
+		<< endl ;
+
+
+	system("Pause");
+	return 0;
+}
